07740f5789929764851374f080a90e6137511a0f
Oscar <oskarromero3@gmail.com>
2018-06-13 13:44:13 +0100

    add export views

--- app/models/export.rb
+++ app/models/export.rb
@@ -1,8 +1,10 @@
 class Export < ApplicationRecord
   belongs_to :shop
 
+  validates_presence_of :name, :time
+
   def self.available_export(time = nil)
-    time ||= Time.zone.now.strftime('%H:%M')
+    time ||= Time.zone.now.strftime('%H:00')
     where(time: time)
   end
 end
