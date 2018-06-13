3e87fa16f1a037e7a9fe96542afed79b8464762d
Oscar <oskarromero3@gmail.com>
2018-06-13 13:49:06 +0100

    minor cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root 'exports#index'
+  resources :exports do
+    collection do
+      get :trigger
+    end
+  end
+
+  mount ShopifyApp::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
